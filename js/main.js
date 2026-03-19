647f6042717a0d50c95ecae40039cf2f93fd9834
vanhao1997 <nguyenvanhaoiuh@gmail.com>
2026-03-19 15:10:57 +0700

    fix: slow down text scramble effect

--- js/main.js
+++ js/main.js
@@ -481,8 +481,8 @@ class TextScramble {
     for (let i = 0; i < length; i++) {
       const from = oldText[i] || '';
       const to = newText[i] || '';
-      const start = Math.floor(Math.random() * 20);
-      const end = start + Math.floor(Math.random() * 20);
+      const start = Math.floor(Math.random() * 50);
+      const end = start + Math.floor(Math.random() * 50);
       this.queue.push({ from, to, start, end });
     }
     cancelAnimationFrame(this.frameRequest);
@@ -499,7 +499,7 @@ class TextScramble {
         complete++;
         output += to;
       } else if (this.frame >= start) {
-        if (!char || Math.random() < 0.28) {
+        if (!char || Math.random() < 0.18) {
           char = this.chars[Math.floor(Math.random() * this.chars.length)];
           this.queue[i].char = char;
         }
